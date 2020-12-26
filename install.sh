69b6daadc7a2beb3e9809a19b9dd59adee57ecf0
Alan Tan <alanwuha91@gmail.com>
2020-12-26 12:03:07 +0000

    feat: split zsh installation into 3 parts

--- install.sh
+++ install.sh
@@ -18,11 +18,13 @@ git config --global user.name = $NAME
 git config --global user.email = $EMAIL
 git config --global alias.hs "log --pretty='%C(yellow)%h %C(cyan)%ad %C(magenta)%an%C(auto)%d %Creset%s' --date=relative --date-order --graph"
 
-# ZSH (Execute `sudo vim /etc/passwd' and modify ubuntu user shell path to /usr/bin/zsh)
+# ZSH
 sudo apt install zsh -y
+
+# OH-MY-ZSH (Execute `sudo vim /etc/passwd' and modify ubuntu user shell path to /usr/bin/zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# ZSH AUTOSUGGESTIONS (Add `plugins=(zsh-suggestions) to .zshrc)
+# ZSH AUTOSUGGESTIONS (Add `plugins=(zsh-suggestions)` to .zshrc)
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # VIM
@@ -31,8 +33,6 @@ cp .vimrc ~/.vimrc
 # NERDTREE
 mkdir -p ~/.vim/pack/vendor/start
 git clone --depth 1 https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
-
-
 
 # GH (Log in using `gh auth login` after installing)
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
